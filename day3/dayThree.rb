ad30c208b0d78335ee4cc2503b6faefabede2113
globalreset <shughes@pobox.com>
2021-12-05 20:08:04 -0600

    adding day3

---
+++ day3/dayThree.rb
@@ -0,0 +1,40 @@
+input = IO.readlines("dayThreeInput.txt").collect { |i| i.chomp.split('').collect{ |j| j.to_i } }
+input2 = [
+   "00100",
+   "11110",
+   "10110",
+   "10111",
+   "10101",
+   "01111",
+   "00111",
+   "11100",
+   "10000",
+   "11001",
+   "00010",
+   "01010"
+].collect { |i| i.split('').collect {|j| j.to_i} }
+
+moreOnes = input.transpose.collect { |i| (i.sum(0.0)/i.size).round(0) }
+
+gamma = 0
+moreOnes.each_index { |i|
+   gamma <<= 1
+   gamma += 1 if(moreOnes[i]==1)
+}
+puts gamma * ((~gamma) & ((1<<moreOnes.size) -1))
+
+bitPos = 0
+o2 = input.clone
+co2 = input.clone
+moreOnes.each_index { |bitPos|
+   moreOnesO2 = o2.transpose.collect { |i| (i.sum(0.0)/i.size).round(0) }
+   if(o2.size>1)
+      o2 = o2.collect { |i| moreOnesO2[bitPos]==i[bitPos] ? i : nil }.compact
+   end
+   moreOnesCO2 = co2.transpose.collect { |i| (i.sum(0.0)/i.size).round(0) }
+   if(co2.size>1)
+      co2 = co2.collect { |i| moreOnesCO2[bitPos]==i[bitPos] ? nil : i }.compact
+   end
+}
+
+puts o2[0].collect {|i| "#{i}" }.reduce(:+).to_i(2)  * co2[0].collect {|i| "#{i}" }.reduce(:+).to_i(2)
