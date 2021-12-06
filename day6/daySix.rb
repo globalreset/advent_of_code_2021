eccecfe976b3cfcaa4e9f992b95f73626a5cab88
globalreset <shughes@pobox.com>
2021-12-06 10:13:52 -0600

    minor cleanup

--- day6/daySix.rb
+++ day6/daySix.rb
@@ -1,35 +1,27 @@
 input = IO.readlines("daySixInput.txt")[0].split(",").collect{ |i| i.to_i }
+
 $descendantCache = {}
 
 def descendantCalc(start, days)
-   if($descendantCache[[start,days]])
-      return $descendantCache[[start,days]]
-   else
-      sum = 0
-      if(days>0)
-         if(start==0)
-            sum = descendantCalc(8, days-1) + descendantCalc(6, days-1)
-         else
-            sum = descendantCalc(start-1, days-1)
-         end
+   return 1 if(days==0)
+   unless($descendantCache.has_key?([start,days]))
+      if(start==0)
+         $descendantCache[[start,days]] = descendantCalc(8, days-1) + descendantCalc(6, days-1)
       else
-         return 1
+         $descendantCache[[start,days]] = descendantCalc(start-1, days-1)
       end
-      $descendantCache[[start,days]] = sum
-      return sum
    end
+   return $descendantCache[[start,days]]
 end
 
 descendantSum = 0
 input.each { |i|
    descendantSum += descendantCalc(i, 80)
 }
-
 puts "80: size=#{descendantSum}"
 
 descendantSum = 0
 input.each { |i|
    descendantSum += descendantCalc(i, 256)
 }
-
 puts "256: size=#{descendantSum}"
