d1a19c78c409ced17f93cca678ecd2339354b375
globalreset <shughes@pobox.com>
2021-12-06 23:17:36 -0600

    adding day seven

---
+++ day7/daySeven.rb
@@ -0,0 +1,19 @@
+input = IO.readlines("daySevenInput.txt")[0].split(",").collect{ |i| i.to_i }
+
+results = {}
+(input.min..input.max).to_a.each { |i|
+   cost = input.inject(0) { |sum, a| sum += (i - a).abs }
+   results[cost] = i
+}
+puts "#{results.keys.min} (#{results[results.keys.min]})"
+
+results = {}
+(input.min..input.max).to_a.each { |i|
+   cost = input.inject(0) { |sum, a| 
+      c = 0
+      (i - a).abs.times { |i| c += i+1 }
+      sum += c 
+   }
+   results[cost] = i
+}
+puts "#{results.keys.min} (#{results[results.keys.min]})"
