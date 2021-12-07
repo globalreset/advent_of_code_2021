860ea11fd5510b2bab38232704bc926c38ac6d56
globalreset <shughes@pobox.com>
2021-12-07 17:21:25 -0600

    testing cached results. summation formula still better, but not by much

--- day7/daySeven.rb
+++ day7/daySeven.rb
@@ -7,13 +7,25 @@ results = {}
 }
 puts "#{results.keys.min} (#{results[results.keys.min]})"
 
+#cache = {}
 results = {}
 (input.min..input.max).to_a.each { |i|
    cost = input.inject(0) { |sum, a| 
-      #c = 0
-      #(i - a).abs.times { |i| c += i+1 }
-      #sum += c 
       n = (i - a).abs
+      # first attempt, rather brute forcey
+      #c = 0
+      #n.times { |i| c += i+1 }
+      #sum += c 
+      
+      # testing same brute force but caching results
+      #if(cache[n]==nil)
+      #   c = 0
+      #   n.times { |i| c += i + 1 }
+      #   cache[n] = c
+      #end
+      #sum += cache[n] 
+
+      # just use formula for simple summation, einstein
       sum += n*(n+1)/2
    }
    results[cost] = i
