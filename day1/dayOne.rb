28c97ea8e930f0724528ad77c0a6853b26bc8d89
globalreset <shughes@pobox.com>
2021-12-08 13:08:30 -0600

    adding another clever solution I found

--- day1/dayOne.rb
+++ day1/dayOne.rb
@@ -4,3 +4,10 @@ puts (1...inputList.size).to_a.inject(0) { |sum,i| (inputList[i] > inputList[i-1
 
 sums = (2...inputList.size).to_a.collect { |i| inputList[i] + inputList[i-1] + inputList[i-2] }
 puts (1...sums.size).to_a.inject(0) { |sum,i| (sums[i]>sums[i-1]) ? sum + 1 : sum }
+
+# adding another solution I saw that solves both in one expression
+# 3-element sliding window works because they only differ by first element of first window and
+# last element of second window.
+[1,3].each { |i|
+   puts inputList[0...-i].zip(inputList[i..-1]).count{|x,y| x<y}
+}
