de1ae9cff63037c334fbc057d48e4f96f3cf37b5
globalreset <shughes@pobox.com>
2021-12-15 12:01:03 -0600

    adding generic dijkstra's to my utility class for future use

--- util/util.rb
+++ util/util.rb
@@ -7,3 +7,40 @@ class Hash
     end
   end
 end
+
+
+# generic dijkstra's search
+# getCost and getNeighbors must be implemented for the graph
+#def getCost(neighbor)
+#  return xyz
+#end
+#def getNeighbors(current)
+#   return [a,b,c]
+#end
+def search(start, goal) 
+   inf = 1/0.0
+   pathHash = {}
+   pathScore = {}
+   pathScore.default = inf
+   pathScore[start] = 0
+   pqueue = [start]
+   while(!pqueue.empty?) do
+      current = pqueue.pop
+      if(current==goal)
+         #printPath(start, goal, pathHash)
+         return pathScore[current]
+      end
+      getNeighbors(current).each { |n|
+         newPathScore = pathScore[current] + getCost(n)
+         if(newPathScore < pathScore[n])
+            pathHash[n] = current
+            pathScore[n] = newPathScore 
+            if(!pqueue.include?(n))
+               pqueue.push(n)
+               # this is pretty slow for the bigger version
+               pqueue.sort! {|a,b| pathScore[b]<=>pathScore[a]}
+            end
+         end
+      }
+   end
+end
